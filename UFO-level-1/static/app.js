482e6a6cad5f926502e4e3a1ea9bc7e493626222
Brandy Knust <brandy_1175@hotmail.com>
2021-04-28 20:13:44 -0500

    4-28 813pm

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -5,9 +5,9 @@ var tableData = data;
 var tbody = d3.select("tbody");
 console.log(data);
 
-data.forEach((sighting) => {
+data.forEach((tableData) => {
     var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => {
+    Object.entries(tableData).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
@@ -29,13 +29,46 @@ button.on("click", runEnter);
 function runEnter() {
     d3.event.preventDefault();
     
-    var inputField = d3.select("#datetime");
-    var inputValue = inputField.property("value");
-
-    console.log(inputValue);
+    var inputField1 = d3.select("#datetime");
+    // var inputField2 = d3.select("#city");
+    // var inputField3= d3.select("#state");
+    // var inputField4 = d3.select("#country");
+    // var inputField5 = d3.select("#shape");
+    // var inputField6 = d3.select("#duration");
+    // var inputField7 = d3.select("#comments");
     
-    var match = tableData.filter(sighting=>sighting.datetime==inputValue);
+    var inputValue1 = inputField1.property("value");
+    // var inputValue2 = inputField2.property("value");
+    // var inputValue3 = inputField3.property("value");
+    // var inputValue4 = inputField4.property("value");
+    // var inputValue5 = inputField5.property("value");
+    // var inputValue6 = inputField6.property("value");
+    // var inputValue7 = inputField7.property("value");
+
+    // console.log([inputValue1, inputValue2, inputValue3, inputValue4, inputValue5, inputValue6, inputValue7]);
+    console.log(inputValue1);
+    
+    var match = tableData.filter(tableData=>tableData.datetime==inputValue1);
+    // var match = tableData.filter(tableData=>tableData.city==inputValue2);
+    // var match = tableData.filter(tableData=>tableData.state==inputValue3);
+    // var match = tableData.filter(tableData=>tableData.country==inputValue4);
+    // var match = tableData.filter(tableData=>tableData.shape==inputValue5);
+    // var match = tableData.filter(tableData=>tableData.duration==inputValue6);
+    // var match = tableData.filter(tableData=>tableData.comments==inputValue7);
 
     console.log(match);
+    var filteredData = match.map(tableData=>match.tableData);
+
+    var table = d3.select("tbody");
+    
+    table.html("");
+
+    match.forEach((match) => {
+      var row = table.append("tr");
+      Object.entries(match).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+      });
+    });
 }
 
