574c203a97b0cf2b180ff76a06349010aa7cd932
Brandy Knust <brandy_1175@hotmail.com>
2021-04-29 18:27:08 -0500

    4-29 630 pm

--- UFO-level-1/static/app.js
+++ UFO-level-1/static/app.js
@@ -6,12 +6,12 @@ var tbody = d3.select("tbody");
 console.log(data);
 
 data.forEach((tableData) => {
-    var row = tbody.append("tr");
-    Object.entries(tableData).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
+  var row = tbody.append("tr");
+  Object.entries(tableData).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
   });
+});
 
 
 
@@ -19,56 +19,72 @@ var button = d3.select("#filter-btn");
 
 
 
-// function dayPlease(match) {
-//     return match.datetime
+// function dayPlease(data) {
+//     return data.datetime
 // };
 
 button.on("click", runEnter);
 // form.on("sumbit", runEnter);
 
 function runEnter() {
-    d3.event.preventDefault();
-    
-    var inputField1 = d3.select("#datetime");
-    // var inputField2 = d3.select("#city");
-    // var inputField3= d3.select("#state");
-    // var inputField4 = d3.select("#country");
-    // var inputField5 = d3.select("#shape");
-    // var inputField6 = d3.select("#duration");
-    // var inputField7 = d3.select("#comments");
-    
-    var inputValue1 = inputField1.property("value");
-    // var inputValue2 = inputField2.property("value");
-    // var inputValue3 = inputField3.property("value");
-    // var inputValue4 = inputField4.property("value");
-    // var inputValue5 = inputField5.property("value");
-    // var inputValue6 = inputField6.property("value");
-    // var inputValue7 = inputField7.property("value");
+  d3.event.preventDefault();
 
-    // console.log([inputValue1, inputValue2, inputValue3, inputValue4, inputValue5, inputValue6, inputValue7]);
-    console.log(inputValue1);
-    
-    var match = tableData.filter(tableData=>tableData.datetime==inputValue1);
-    // var match = tableData.filter(tableData=>tableData.city==inputValue2);
-    // var match = tableData.filter(tableData=>tableData.state==inputValue3);
-    // var match = tableData.filter(tableData=>tableData.country==inputValue4);
-    // var match = tableData.filter(tableData=>tableData.shape==inputValue5);
-    // var match = tableData.filter(tableData=>tableData.duration==inputValue6);
-    // var match = tableData.filter(tableData=>tableData.comments==inputValue7);
+  var data = tableData;
 
-    console.log(match);
-    var filteredData = match.map(tableData=>match.tableData);
+  var inputField1 = d3.select("#datetime");
+  var inputField2 = d3.select("#city");
+  var inputField3 = d3.select("#state");
+  var inputField4 = d3.select("#country");
+  var inputField5 = d3.select("#shape");
+  var inputField6 = d3.select("#duration");
+
+
+  var inputValue1 = inputField1.property("value");
+  var inputValue2 = inputField2.property("value");
+  var inputValue3 = inputField3.property("value");
+  var inputValue4 = inputField4.property("value");
+  var inputValue5 = inputField5.property("value");
+  var inputValue6 = inputField6.property("value");
+
+  console.log([inputValue1, inputValue2, inputValue3, inputValue4, inputValue5, inputValue6]);
+  // console.log(inputValue1);
+  if (inputField1 != "") {
+    data = data.filter(obj => obj.datetime == inputValue1);
+    console.log(data);
+  }
+  if (inputField2 != "") {
+    data = data.filter(obj => obj.city == inputValue2);
+    console.log(data);
+  }
+  if (inputField3 != "") {
+    data = data.filter(obj => obj.state == inputValue3);
+    console.log(data);
+  }
+  if (inputField4 != "") {
+    data = data.filter(obj => obj.country == inputValue4);
+    console.log(data);
+  }
+  if (inputField5 != "") {
+    data = data.filter(obj => obj.shape == inputValue5);
+    console.log(data);
+    if (inputField6 != "") {
+      data = data.filter(obj => obj.durationMinutes == inputValue6);
+      console.log(data);
+    }
+
+
+    console.log(data);
 
     var table = d3.select("tbody");
-    
+
     table.html("");
 
-    match.forEach((match) => {
+    data.forEach((data) => {
       var row = table.append("tr");
-      Object.entries(match).forEach(([key, value]) => {
+      Object.entries(data).forEach(([key, value]) => {
         var cell = row.append("td");
         cell.text(value);
       });
     });
+  }
 }
-
